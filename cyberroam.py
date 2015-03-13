3742f14e9e527e964ab90fa2bc6d9ee9d6c87b0b
Shuchit Gandhi <shuchit95@gmail.com>
2015-03-13 17:06:18 +0530

    Script for Auto-Login

---
+++ cyberroam.py
@@ -0,0 +1,30 @@
+# Developed by shuchit gandhi
+
+cyberroamIP = "10.100.56.55" #The IP of the Cyberroam site.
+cyberroamPort = "8090" #Set to "" if not using.
+sleeptime = 7100 #Seconds to sleep
+
+import getpass
+from time import strftime
+import urllib, time, sys
+
+
+passwd = "ceid$321"
+#username="201101211@da-iict.org"
+username="CEID-Zone"
+cyberroamAddress = cyberroamIP
+if cyberroamPort != "":
+	cyberroamAddress = cyberroamAddress+":"+cyberroamPort
+
+while True:
+	try:
+		f = urllib.urlopen("http://"+cyberroamAddress+"/httpclient.html","mode=191&username="+username+"&password="+passwd+"&btnSubmit=Login")	
+		data = f.read()
+		print "Message from server: "+data[data.rfind("""display:none">""")+14:-27]
+		time.sleep(sleeptime)
+	except KeyboardInterrupt:
+		f = urllib.urlopen("http://"+cyberroamAddress+"/httpclient.html","mode=193&username="+username+"&password="+passwd+"&btnSubmit=Logout")
+		data = f.read()
+		print "Message from server: "+data[data.rfind("""display:none">""")+14:-27]
+		sys.exit(0)
+		
